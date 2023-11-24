0ae8a0d148e479745f814adee17b6c1a89512484
Aryan Tiwari <80637463+17Aryan@users.noreply.github.com>
2023-11-24 12:50:15 +0530

    Time: 0 ms (100.00%), Space: 9 MB (95.98%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size()-1;
+        for(int i=n;i>=0;i--){
+            if(digits[i]!=9){
+                digits[i]=digits[i]+1;
+                break;
+            }
+            else
+                digits[i]=0;
+        }
+        if(digits[0]==0)
+            digits.insert(digits.begin(),1);
+        
+        return digits;
+    }
+};
